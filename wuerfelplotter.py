e0ef240781b79596d8968cb093f62c752d20659c
ColonelRoyMustang <94714084+ColonelRoyMustang@users.noreply.github.com>
2021-11-19 20:41:53 +0100

    Update wuerfelplotter.py

--- wuerfelplotter.py
+++ wuerfelplotter.py
@@ -1,73 +1,74 @@
+#!/usr/bin/env python3
+
+# Imports
 from functools import lru_cache
 import matplotlib.pyplot as plt
 
-
-
-#von Stackoverflow geklaut....shame on me
-
 while True:
-    rolls = input("How many rolls? ")
-    if rolls.isdigit():
-        break
-    print("Please input only integers!")
-while True:
-    sides = input("How many sides? ")
-    if sides.isdigit():
-        break
-    print("Please input only integers!")
+    # Determining input
+    print("----------------------------------------------------\n"
+          "Welcome to the ndm dice sums propability calculator.\n"
+          "This shows you the probability for your dice roll,\n"
+          "the most probable one(s) and plots all probabilites.\n"
+          "Please make sure to only input integers.\n"
+          "----------------------------------------------------\n")
+    while True:
+        sides = input("How many sides? ")
+        if sides.isdigit():
+            sides = int(sides)
+            if sides > 0:
+                break
+        print("Please input only positive integers!")
+    while True:
+        rolls = input("How many rolls? ")
+        if rolls.isdigit():
+            rolls = int(rolls)
+            if rolls > 0:
+                break
+        print("Please input only positive integers!")
+    while True:
+        playersum = input("What is your sum? ")
+        if playersum.isdigit():
+            playersum = int(playersum)
+            if playersum >= rolls and playersum <= sides * rolls:
+                break
+            print("This sum isn't possible with your specified roll.")
+        else:
+            print("Please input only integers!")
 
-print(f"You are going to simulate a {rolls}d{sides} roll!")
-while True:
-    playersum = input("What is your sum? ")
-    if playersum.isdigit():
-        break
-    print("Please input only integers!")
-sides = int(sides)
-rolls = int(rolls)
-playersum = int(playersum)
+    print("\n\n\n")
+    # Calculation functions
+    @lru_cache(None)
+    def sum_freq(total, rolls, faces):
+        if not rolls:
+            return not total
+        return sum(sum_freq(total - die, rolls - 1, faces) for die in range(1, faces + 1))
 
-values = {}
-@lru_cache(None)
-def sum_freq(total, rolls, faces):
-    if not rolls:
-        return not total
-    return sum(sum_freq(total - die, rolls - 1, faces)
-               for die in range(1, faces + 1))
-def probability_calculator(roll_total, num_of_rolls, dice_faces):
-    return sum_freq(roll_total, num_of_rolls, dice_faces)/ dice_faces ** num_of_rolls
-for i in range(rolls,rolls*sides+1):
-    values[i] = probability_calculator(i, rolls, sides)
+    def probability_calculator(roll_total, num_of_rolls, dice_faces):
+        return sum_freq(roll_total, num_of_rolls, dice_faces) / dice_faces ** num_of_rolls
 
+    # Preparing plot data
+    sums = []
+    probabilities = []
+    for i in range(rolls, rolls * sides + 1):
+        sums.append(i)
+        probabilities.append(round(probability_calculator(i, rolls, sides) * 100, 6))
+    info_str = "The probability of your throw was {} %\n".format(probabilities[playersum - rolls])
 
-xsum = []
-yprobability = []
+    # Finding maximum/a
+    maximum = max(probabilities)
+    max_ind = probabilities.index(maximum)
+    if probabilities[max_ind + 1] == maximum:
+        info_str += "The highest probability is rolling a {} with a probability of {} %".format(sums[max_ind], maximum)
+    else:
+        info_str += "The highest probabilities are for rolling a {} or {} with a probability of {} %".format(sums[max_ind], sums[max_ind] + 1, maximum)
 
-for i in values.keys():
-    xsum.append(i)
-    yprobability.append(values[i])
-
-maximum = 0
-listofmax = []
-safe = 0
-for i in range(len(yprobability)):
-    if yprobability[i] >= maximum:
-        maximum = round(yprobability[i],4)
-        listofmax.append(maximum)
-        safe = i
-
-
-
-plt.plot(xsum,yprobability,"r+")
-
-if listofmax[-1] != listofmax[-2]:
-    plt.title(f"The highest probability is rolling a {xsum[safe]} with a probability of\n{maximum*100} %",
-             color="#330000", family="serif", fontsize=8)
-
-else:
-    plt.title(f"The highest probabilities are for rolling {xsum[safe-1]} and {xsum[safe]} with a probability of\n{maximum*100} %"
-,
-        color="#330000", family="serif", fontsize=8)
-
-plt.plot([playersum,playersum],[0, maximum])
-
-plt.show()
+    # Plotting
+    plt.title("Dice sum probabilities")
+    plt.xlabel("Dice sum\n\n" + info_str)
+    plt.ylabel("Probability / %")
+    #plt.text(sums[len(sums) - 1] / 2, probabilities[1], info_str, horizontalalignment='center') # For putting it in the graph itself
+    plt.plot(sums, probabilities, "r+")
+    plt.vlines(playersum, 0, maximum)
+    plt.tight_layout()
+    plt.show()
